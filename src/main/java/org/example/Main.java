ba60677dcb7b4634bdc9ce3325005d041e160278
gwang <ahngwangeun@gmail.com>
2025-12-29 20:26:43 +0900

    gwang_fetch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         System.out.printf("I'm gwang!");
+        System.out.printf("I'm gwang!");
     }
 }
